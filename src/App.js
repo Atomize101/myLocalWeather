d9eb3df354a0a2115deaf5f3faed930bbd5294ab
Chris Patrick <atomize101@gmail.com>
2020-10-15 14:30:16 -0400

    changing to use react hooks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import React, { Component } from 'react';
-import WeekForcast from './components/WeekForcast';
+import React,{useState} from 'react';
+import Form from './components/Form';
+import Weather from './components/Weather';
+import apiConfig from './apiKey'
 
-class App extends Component {
-  render() {
+function App() {
+    const [weather,setWeather] = useState([])
+  
+    async function fetchData(e) {
+        e.preventDefault()
+      const apiData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=${apiConfig.openWeatherKey}`)
+        .then( res => res.json())
+        .then(data => data)
+        setWeather({
+          data: apiData
+        }
+        )
+    }
+  
     return (
       <div className="App">
-        <WeekForcast />
+        <h3>WEATHER APP</h3>
+        <Form getWeather={fetchData} />
+        {console.log(weather.data)}
       </div>
     );
   }
-}
-
-export default App;
+  
+  export default App;
--- src/components/WeekForcast.js
+++ src/components/WeekForcast.js
@@ -6,7 +6,6 @@ class WeekForcast extends React.Component {
     const weatherURL =
     `http://api.openweathermap.org/data/2.5/forecast?zip=11102&units=imperial&APPID=${apiConfig.openWeatherKey}`
 
-    console.log(weatherURL);
     return (
       <div>
         <h1>My weather app</h1>     
